382bcce666b30c7f5469125bf358c55ae25eb5f0
seojun-alpha <159983818+seojun-alpha@users.noreply.github.com>
2024-05-28 21:34:24 +0900

    Delete TROLLGG directory

--- TROLLGG/summoner/forms.py
+++
@@ -1,7 +0,0 @@
-# summoner/forms.py
-
-from django import forms
-
-class SummonerSearchForm(forms.Form):
-    riot_id = forms.CharField(label='Riot ID', max_length=50)
-    tag_line = forms.CharField(label='Tag Line', max_length=50)
